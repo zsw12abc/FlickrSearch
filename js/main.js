83f7cbc8c1ae556be5b964e5c67d43c92ca14d6f
Shaowei <zsw22@vip.qq.com>
2017-11-22 16:38:59 +1100

    clean the div and deal with the text

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $("#search").click(function () {
+    $("#results").empty();
     $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
         {
             tags: $("#searchterm").val(),
@@ -8,13 +9,26 @@ $("#search").click(function () {
         function (data) {
             $.each(data.items, function (i, item) {
                 var img = $("<img>").attr("src", item.media.m).addClass("card-img-top");
-                var author = $("<h4>").addClass("card-title").text("Author: " + item.author);
-                var tags = $("<p>").addClass("card-text").text("Tags: " + item.tags);
+                var authorString = item.author.split("(")[1];
+                var authorFinal = authorString.substring(1, authorString.length - 2);
+                var tagArray = item.tags.split(" ");
+                var tagFinalArray = new Array();
+                for (var j = 0; j < tagArray.length; j++) {
+                    var obj = "#" + tagArray[j];
+                    // console.log(obj);
+                    tagFinalArray.push(obj);
+                }
+                var tagFinal = tagFinalArray.join(" ");
+                // console.log(tagFinalArray);
+
+                var author = $("<h4>").addClass("card-title").text("Author: " + authorFinal);
+                var tags = $("<p>").addClass("card-text").text(tagFinal);
                 var btnLink = $("<a>").addClass("btn btn-primary").attr("href", item.link).text("See It");
                 var boxBody = $("<div>").addClass("card-body").append(author, tags, btnLink);
                 var itemBox = $("<div>").addClass("card col-4").append(img, boxBody);
                 $("#results").append(itemBox);
-                if (i == 10) return false;
+                // console.log(i);
+                // if (i > 100) return false;
             });
         });
 });
